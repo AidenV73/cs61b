0c8574e7204b38e674d0431c7d735e96394b176b
Aiden <aidenvoon73@icloud.com>
2026-01-06 16:24:07 +0800

    Done with Optional Exercise

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -2,6 +2,7 @@ package bstmap;
 
 import org.w3c.dom.Node;
 
+import java.awt.color.ICC_ColorSpace;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -20,6 +21,10 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         public BSTNode(K k, V v) {
             key = k;
             value = v;
+        }
+
+        public boolean haveChild() {
+            return (left != null || right != null);
         }
     }
 
@@ -47,7 +52,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /** Helper method to check key exist in map */
     private boolean containsKey(BSTNode node, K key) {
         // If key is not inside tree
-        if (node.key == null) {
+        if (node == null) {
             return false;
         }
         // If key equals to current node then return true
@@ -78,7 +83,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /** Helper method to get key value pair */
     private V get(BSTNode node, K key) {
         // If not found
-        if (node.key == null) {
+        if (node == null) {
             return null;
         }
         // If key equals to current node
@@ -174,18 +179,110 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         return keys;
     }
 
+    private V removedValue;
 
     /** Remove node in BST and return its value */
     @Override
     public V remove(K key) {
-        return remove(root, key);
+        root = remove(root, key);
+        return removedValue;
     }
 
-    private V remove (BSTNode node, K key) {
-        // Somehow need to maintain balance (Find the node that have no child ?? left->right leaf or right -> left leaf)
-        // If remove node that have single child
-        // If remove node that have two child
+    /** Helper method to switch node */
+    private BSTNode remove (BSTNode node, K key) {
+        // Recursively find node that are going to be deleted
+        // The node that are going to be removed are not exist in current tree
+        if (node == null) {
+            return null;
+
+        } else {
+            // If key is greater than current node.key
+            if (key.compareTo(node.key) > 0) {
+                node.right = remove(node.right, key);
+            }
+            // If key is smaller than current node.key
+            else if (key.compareTo(node.key) < 0) {
+                node.left = remove(node.left, key);
+            }
+
+            // If key is founded then do deletion
+            else {
+                removedValue = node.value;
+                // If key is leaf
+                if (node.left == null && node.right == null) {
+                    node = null;
+                    size -= 1;
+                    return null;
+                }
+
+                // If node has right child only
+                if (node.left == null) {
+                    size -= 1;
+                    return node.right;
+                }
+
+                // If node has left child only
+                else if (node.right == null) {
+                    size -= 1;
+                    return node.left;
+                }
+
+                // If node have two children
+                else {
+                    BSTNode targetNode;
+                    // If pathCountRight longer than pathCountLeft
+                    if (pathCountRight(node) > pathCountLeft(node)) {
+                        targetNode = findMinimum(node.right);
+                        node.key = targetNode.key;
+                        node.value = targetNode.value;
+                        node.right = remove(node.right, targetNode.key);
+                    } else {
+                        targetNode = findMaximum(node.left);
+                        node.key = targetNode.key;
+                        node.value = targetNode.value;
+                        node.left = remove(node.left, targetNode.key);
+                    }
+                }
+            }
+        }
+        return node;
     }
+    /** Helper method to get the minimum node of path */
+    public BSTNode findMinimum(BSTNode node) {
+        if (node.left == null) {
+            return node;
+        } else {
+            return findMinimum(node.left);
+        }
+    }
+
+    /** Helper method to get the maximum node of path */
+    public BSTNode findMaximum(BSTNode node) {
+        if (node.right == null) {
+            return node;
+        } else {
+            return findMaximum(node.right);
+        }
+    }
+
+    /** Helper method to count path left*/
+    public Integer pathCountLeft(BSTNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + pathCountLeft(node.left);
+        }
+    }
+
+    /** Helper method to count path right */
+    public Integer pathCountRight(BSTNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + pathCountRight(node.right);
+        }
+    }
+
     @Override
     public V remove(K key, V value) {
         throw new UnsupportedOperationException();
